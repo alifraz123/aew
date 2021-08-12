93c73930eb9b74dbd72d47918d3f14f581e609e9
umar habib <umarhaabib98@gmail.com>
2021-08-12 14:45:05 +0500

    changew

--- resources/views/admin/modules/SalesBook/salesbookedit.blade.php
+++ resources/views/admin/modules/SalesBook/salesbookedit.blade.php
@@ -2,13 +2,45 @@
 @section('content')
 
 <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Sales Invoice Edit</h1>
+                </div>
+                
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
 
+    <!-- Main content -->
     <section class="content">
-        <div style="margin-top: 1rem;" class="container-fluid">
-        
-        This is edit salesbook invoice data
-         
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-outline card-info">
+                    <div class="card-header">
+                        <h3 class="card-title">
+                            Summernote
+                        </h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <textarea id="summernote">
+                Place <em>some</em> <u>text</u> <strong>here</strong>
+              </textarea>
+                    </div>
+                    <div class="card-footer">
+                        Visit <a href="https://github.com/summernote/summernote/">Summernote</a> documentation for more examples and information about the plugin.
+                    </div>
+                </div>
+            </div>
+            <!-- /.col-->
         </div>
+        <!-- ./row -->
+      
+        <!-- ./row -->
+    </section>
+    <!-- /.content -->
 </div>
-</section>
 @endsection
